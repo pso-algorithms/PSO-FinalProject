f87d73fde5c582ce997f486053cbe2e4da789ac8
Aravind-Mandapaka <mandapaka.a@husky.neu.edu>
2019-08-16 12:19:51 -0400

    BloodGroup_Class

--- src/pso_final/BloodGroup.java
+++ src/pso_final/BloodGroup.java
@@ -5,10 +5,31 @@
  */
 package pso_final;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 /**
  *
  * @author aravi
  */
-public class BloodGroup {
+public enum BloodGroup {
     
+       APOSITIVE,
+    ANEGETIVE,
+    BPOSITIVE,
+    BNEGATIVE,
+    OPOSITIVE,
+    ONEGATIVE,
+    ABPOSITIVE,
+    ABNEGATIVE;
+    
+  private static final List<BloodGroup> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
+  private static final int SIZE = VALUES.size();
+  private static final Random RANDOM = new Random();
+  
+  public static BloodGroup randomBloodgroup()  {
+    return VALUES.get(RANDOM.nextInt(SIZE));
+  }
 }
